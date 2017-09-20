483f76305fe8cc458cdc41ff4548301471444031
Toma Alex <intern@intern12>
2017-09-20 14:21:29 +0300

    Toma

--- app/src/main/java/Fragments/DateGeneraleFragment.java
+++ app/src/main/java/Fragments/DateGeneraleFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckedTextView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.intern.myapplication.R;
 
@@ -34,29 +35,38 @@ public class DateGeneraleFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       // RelativeLayout buttons = (RelativeLayout) getActivity().findViewById(R.layout.bottom_buttons);
+       
 
         getActivity().setTitle("Date Generale");
         Bundle bundle = this.getArguments();
         Factura factura = bundle.getParcelable("object");
 
-        CheckedTextView dtEst = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView);
-        CheckedTextView dtEm = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView7);
-        CheckedTextView serieFac = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView9);
-        CheckedTextView responsabil = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView10);
-        CheckedTextView moneda = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView11);
-        CheckedTextView TVA = (CheckedTextView) getActivity().findViewById(R.id.checkedTextView12);
+        TextView dtEst = (TextView) getActivity().findViewById(R.id.dtEst);
+        TextView dtEm = (TextView) getActivity().findViewById(R.id.dtEm);
+        TextView serieCod = (TextView) getActivity().findViewById(R.id.serieCod);
+        TextView serieSec = (TextView) getActivity().findViewById(R.id.serieSec);
+        TextView responsabil = (TextView) getActivity().findViewById(R.id.responsabil);
+        TextView moneda = (TextView) getActivity().findViewById(R.id.moneda);
+        TextView TVA = (TextView) getActivity().findViewById(R.id.tva);
+        TextView status = (TextView) getActivity().findViewById(R.id.status);
+        TextView dtScad = (TextView) getActivity().findViewById(R.id.dtScad);
+        TextView creatDe = (TextView) getActivity().findViewById(R.id.creatDe);
+        TextView validatDe = (TextView) getActivity().findViewById(R.id.validatDe);
+        TextView emisDe = (TextView) getActivity().findViewById(R.id.emisDe);
 
 
         dtEst.setText(DateFormat.getDateInstance().format(factura.getDtEstimata()));
         dtEm.setText(DateFormat.getDateInstance().format(factura.getDtEmitere()));
-        serieFac.setText(String.valueOf(factura.getSerieFactura().getCod()) + String.valueOf(factura.getSerieFactura().getSecventa()));
+        serieCod.setText(String.valueOf(factura.getSerieFactura().getCod()));
+        serieSec.setText(String.valueOf(factura.getSerieFactura().getSecventa()));
         responsabil.setText(String.valueOf(factura.getAngajat().getNume()));
         moneda.setText(String.valueOf(factura.getMoneda().getNume()));
         TVA.setText(String.valueOf(factura.getCotaTVA().getProcent()));
-
-        
-
+        status.setText(String.valueOf(factura.getStatusFactura().getStatus()));
+        dtScad.setText(DateFormat.getDateInstance().format(factura.getDtScadenta()));
+        creatDe.setText(String.valueOf(factura.getCreatDe().getNume()));
+        validatDe.setText(String.valueOf(factura.getValidatDe().getNume()));
+        emisDe.setText(String.valueOf(factura.getEmisDe()));
     }
 
 }
