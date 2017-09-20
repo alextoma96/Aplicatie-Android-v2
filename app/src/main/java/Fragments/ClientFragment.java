301cc115d449fb01903a054d07eb66797f1e88d5
Toma Alex <intern@intern12>
2017-09-20 14:43:53 +0300

    Toma

--- app/src/main/java/Fragments/ClientFragment.java
+++ app/src/main/java/Fragments/ClientFragment.java
@@ -3,25 +3,45 @@ package Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.intern.myapplication.R;
 
+import Commons.Client;
+
 public class ClientFragment extends Fragment {
 
-    public ClientFragment() { }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_client, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Client");
+        Bundle bundle = this.getArguments();
+        Client client = bundle.getParcelable("object");
+
+        TextView numComp = (TextView) getActivity().findViewById(R.id.numClient);
+        TextView codIn = (TextView) getActivity().findViewById(R.id.codInC);
+        TextView codFis = (TextView) getActivity().findViewById(R.id.codFisC);
+        TextView banca = (TextView) getActivity().findViewById(R.id.bancaC);
+        TextView codIBAN = (TextView) getActivity().findViewById(R.id.codIBANC);
+        TextView adresa = (TextView) getActivity().findViewById(R.id.adresaC);
+
+        numComp.setText(String.valueOf(client.getNume()));
+        codIn.setText(String.valueOf(client.getCodInregistrare()));
+        codFis.setText(String.valueOf(client.getCodFiscal()));
+        banca.setText(String.valueOf(client.getBanca()));
+        codIBAN.setText(String.valueOf(client.getIban()));
+        adresa.setText(String.valueOf(client.getAdresa()));
+    }
+
 }
