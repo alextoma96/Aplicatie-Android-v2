38a36c63a1beb438ae8883e9142086fe36543073
Andrei <stamate.cristian97@yahoo.com>
2017-09-22 15:52:12 +0300

    Andrei

--- app/src/main/java/com/example/intern/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/intern/myapplication/LoginActivity.java
@@ -3,23 +3,17 @@ package com.example.intern.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -27,7 +21,6 @@ import java.util.List;
 
 import Commons.Utilizator;
 import Networking.HttpConnectionUtilizatori;
-import Utils.Constant;
 
 public class LoginActivity extends Fragment {
 
@@ -148,11 +141,6 @@ public class LoginActivity extends Fragment {
         };
         connection.execute("http://" + PreferenceManager.getDefaultSharedPreferences(getContext()).getString("ip", "192.168.8.98") + "/kepres204/api/rs/utilizator/list");
     }
-
-    @Override
-    public void onPrepareOptionsMenu(Menu menu) {
-        menu.findItem(R.id.nav_logout).setVisible(false);
-        super.onPrepareOptionsMenu(menu);
-    }
+    
 
 }
