392c2c9e870bac12f82d8a8f62ea55895687ebad
idanciu <iulia.danciu@kepler-rominfo.com>
2017-09-25 12:30:33 +0300

    iulia

--- app/src/main/java/Commons/DestinatarMesaj.java
+++ app/src/main/java/Commons/DestinatarMesaj.java
@@ -8,23 +8,23 @@ import android.os.Parcelable;
  */
 
 public class DestinatarMesaj implements Parcelable {
-    private String nume;
-    private String email;
-    private String telefon;
-    private Mesaj mesaj;
+    private Integer idMesaj;
+    private Integer idAngajat;
 
-    public DestinatarMesaj(String nume, String email, String telefon, Mesaj mesaj) {
-        this.nume = nume;
-        this.email = email;
-        this.telefon = telefon;
-        this.mesaj = mesaj;
+    public Integer getIdMesaj() { return idMesaj; }
+    public DestinatarMesaj setIdMesaj(Integer idMesaj) { this.idMesaj = idMesaj; return this;}
+
+    public Integer getIdAngajat() { return idAngajat; }
+    public DestinatarMesaj setIdAngajat(Integer idAngajat) { this.idAngajat = idAngajat; return this;}
+
+    public DestinatarMesaj(Integer idMesaj, Integer idAngajat) {
+        this.idMesaj = idMesaj;
+        this.idAngajat = idAngajat;
     }
 
     protected DestinatarMesaj(Parcel in) {
-        nume = in.readString();
-        email = in.readString();
-        telefon = in.readString();
-        mesaj = in.readParcelable(Mesaj.class.getClassLoader());
+        idMesaj = in.readInt();
+        idAngajat = in.readInt();
     }
 
     public static final Creator<DestinatarMesaj> CREATOR = new Creator<DestinatarMesaj>() {
@@ -35,39 +35,12 @@ public class DestinatarMesaj implements Parcelable {
         public DestinatarMesaj[] newArray(int size) { return new DestinatarMesaj[size]; }
     };
 
-    public String getNume() { return nume; }
-    public DestinatarMesaj setNume(String nume) { this.nume = nume; return this;}
-
-    public String getEmail() { return email; }
-    public DestinatarMesaj setEmail(String email) { this.email = email; return this;}
-
-    public String getTelefon() { return telefon; }
-    public DestinatarMesaj setTelefon(String telefon) { this.telefon = telefon; return this; }
-
-    public Mesaj getAngajat() { return mesaj; }
-    public DestinatarMesaj setMesaj(Mesaj mesaj) { this.mesaj = mesaj; return this;}
-
     @Override
     public int describeContents() { return 0; }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(nume);
-        dest.writeString(email);
-        dest.writeString(telefon);
-        dest.writeParcelable(mesaj, flags);
-    }
-
-    @Override
-    public String toString() {
-        String destinatar = "Nume: " + nume +
-                ", \nE-mail: " + email +
-                ", \nTelefon: " + telefon +
-                ", \nMesaj: ";
-        if (mesaj != null && mesaj.getContinut() != null)
-            destinatar = destinatar + mesaj.getContinut();
-        else
-            destinatar = destinatar + "null";
-        return   destinatar;
+        dest.writeInt(idMesaj);
+        dest.writeInt(idAngajat);
     }
 }
--- app/src/main/java/Networking/HttpConnectionDestinatarMesaj.java
+++ app/src/main/java/Networking/HttpConnectionDestinatarMesaj.java
@@ -1,8 +1,6 @@
 package Networking;
 
-import android.util.Log;
-
-import com.example.intern.myapplication.StatusActivity;
+import android.os.AsyncTask;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,33 +14,24 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-import Commons.Angajat;
-import Commons.CotaTVA;
 import Commons.DestinatarMesaj;
-import Commons.Mesaj;
-import Commons.Moneda;
-
-import static Utils.Constant.SIMPLE_DATE_FORMAT;
 
 /**
  * Created by intern on 9/22/2017.
  */
 
-public class HttpConnectionDestinatarMesaj  {
+public class HttpConnectionDestinatarMesaj extends AsyncTask<String, Void, ArrayList<DestinatarMesaj>> {
 
     URL url;
     HttpURLConnection connection;
-    SimpleDateFormat dateFormat = SIMPLE_DATE_FORMAT;
 
     @Override
     protected ArrayList<DestinatarMesaj> doInBackground(String... params) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
             url = new URL(params[0]);
-            Log.i("url", url.toString());
             connection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = connection.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -74,34 +63,14 @@ public class HttpConnectionDestinatarMesaj  {
         JSONArray jsonArray = new JSONArray(JSONString);
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonDestinatarMesaj = jsonArray.getJSONObject(i);
-            Integer id = jsonDestinatarMesaj.getInt("id");
+            JSONObject jsonMesaj = jsonDestinatarMesaj.getJSONObject("mesaj");
+            Integer idMesaj = jsonMesaj.getInt("id");
+            JSONObject jsonDestinatar = jsonDestinatarMesaj.getJSONObject("destinatar");
+            Integer idDestinatar = jsonDestinatar.getInt("id");
 
-
-            JSONObject jsonMesaj = jsonMesaj.getJSONObject("mesaj");
-            Mesaj mesaj = parseMesaj(jsonMesaj);
-
-            JSONObject jsonAngajat = jsonAngajat.getJSONObject("angajat");
-            Angajat angajat = parseAngajat(jsonAngajat);
-
+            DestinatarMesaj destinatarMesaj = new DestinatarMesaj(idMesaj, idDestinatar);
+            listaDestinatarMesaj.add(destinatarMesaj);
         }
-
-        }
-        // return listaDestinatarMesaj;
+         return listaDestinatarMesaj;
     }
-
-    private Mesaj parseMesaj(JSONObject object) throws  JSONException {
-        String titlu = object.getString("titlu");
-        String continut = object.getString("continut");
-        String trimis = object.getString("trimis");
-        String citit = object.getString("citit");
-    }
-    private Angajat parseAngajat(JSONObject object) throws JSONException {
-        String email = object.getString("email");
-        String cod = object.getString("cod");
-        String telefon = object.getString("telefon");
-        String nume = object.getString("nume");
-        String memo = object.getString("memo");
-        return new Angajat(cod, nume, memo, email, telefon);
-    }
-
 }
